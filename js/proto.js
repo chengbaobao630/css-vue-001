df05a072357d45c2d19daf783edb5e90de63f54b
cheng.cheng <chengbaobao630@hotmail.com>
2017-09-29 10:57:06 +0800

    update

--- js/proto.js
+++ js/proto.js
@@ -1,61 +1,21 @@
-//function Person(name){
-//	this.name = name;
-//}
-//var p = new Person();
-//alert(p.__proto__ == Person.prototype)
-//alert(Person.prototype.constructor.prototype.constructor.prototype)
-//alert(p.__proto__.constructor)
-//alert(Person.prototype.__proto__)
-//alert(Person.__proto__)
-//
-//Person.prototype.say = function(){
-//	console.log(this.name);
-//}
-//
-//p.say();
-//function Student(name,age){
-//		Person.call(this,name);
-//		this.age = age;
-//}
-//
-//function inherits(Child, Parent) {
-//  var F = function () {};
-//  F.prototype = Parent.prototype;
-//  Child.prototype = new F();
-//  Child.prototype.constructor = Child;
-//}
-//
-//class Teacher extends Person{
-//	constructor(name,grade){
-//		super(name);
-//		this.grade = grade;
-//	}
-//	
-//}
-//inherits(Student,Person)
-//var s = new Student("cc",20);
-//s.say();
-//var t = new Teacher("dd",20);
-//t.say();
-
 var vue = new Vue({
 	el:"#name",
 	data:{
 		name:"金闪闪"
 	}
-})
+});
 var app2 = new Vue({
   el: '#v-bind',
   data: {
     message: '页面加载于 ' + new Date().toLocaleString()
   }
-})
+});
 var app3 = new Vue({
   el: '#app-3',
   data: {
     seen: true
   }
-})
+});
 var app4 = new Vue({
   el: '#app-4',
   data: {
@@ -65,7 +25,8 @@ var app4 = new Vue({
       { text: '整个牛项目' }
     ]
   }
-})
+});
+app4.todos.push({text:'呵呵哒'});
 var app5 = new Vue({
   el: '#app-5',
   data: {
@@ -76,17 +37,17 @@ var app5 = new Vue({
       this.message = this.message.split('').reverse().join('')
     }
   }
-})
+});
 var app6 = new Vue({
   el: '#app-6',
   data: {
     message: 'Hello Vue!'
   }
-})
+});
 Vue.component('todo-item', {
   props: ['todo'],
   template: '<li>{{ todo.text }}</li>'
-})
+});
 var app7 = new Vue({
   el: '#app-7',
   data: {
@@ -96,4 +57,23 @@ var app7 = new Vue({
       { id: 2, text: '随便其他什么人吃的东西' }
     ]
   }
-})
+});
+app7.groceryList.push({id:3,text:'呵呵哒'});
+// $watch 是一个实例方法
+vue.$watch('name', function (newValue, oldValue) {
+    console.log(newValue);
+    console.log(oldValue);
+});
+app4.$watch('todos', function (newValue, oldValue) {
+    console.log(newValue);
+    console.log(oldValue);
+});
+document.getElementsByClassName("dropdown")[0].addEventListener("mouseover" , function () {
+    document.getElementsByClassName("ul-dropdown")[0].style.display="block";
+},false);
+
+document.getElementsByClassName("dropdown")[0].onmouseout = function () {
+    document.getElementsByClassName("ul-dropdown")[0].style.display="none";
+}
+
+
